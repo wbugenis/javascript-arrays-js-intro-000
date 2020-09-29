8a93b61b18a87f7242c89464aeda5db30b19839b
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-29 01:18:12 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr, s) {
+  arr.unshift(s);
+  return arr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, s) {
+  arr = arr.shift();
+  arr.unshift(s);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, s) {
+  return arr.push(s)
+}
+
+function destructivelyAddElementToEndOfArray(arr, s) {
+  arr.last() = s;
+  return arr;
+}
+
+function accessElementInArray(arr, i) {
+ return arr[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  return array.shift();
+}
