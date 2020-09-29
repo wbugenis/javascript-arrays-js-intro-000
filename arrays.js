a2464eb6bca3a5178e67fc75d154e4b1f0749860
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-29 01:28:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, s) {
-  var array = [s, ...arr]  
+  var array = [s, ...arr]
   return array;
 }
 
